f0ed666fc720fd142bf295751a7ac4f956c24875
Frédéric Boyer <fboyer@gmail.com>
2016-01-06 16:55:56 -0500

    Completed some of the sub functionality required for Day 5

--- src/Day5/DoesntHeHaveInternElvesForThis.hs
+++ src/Day5/DoesntHeHaveInternElvesForThis.hs
@@ -5,26 +5,35 @@ module Day5.DoesntHeHaveInternElvesForThis
 import Data.List as L
 import Data.List.Split as S
 
--- countNiceStrings :: String -> Int
--- countNiceStrings = undefined -- filter doesNotContainDisallowedStrings . filter containsAtLeastOneLetterThatAppearsTwiceInARow . filter hasAtLeastThreeVowels $ S.splitOn "\n" input
---
--- isNice :: String -> Bool
--- isNice = undefined --filter doesNotContainDisallowedStrings . containsAtLeastOneLetterThatAppearsTwiceInARow . hasAtLeastThreeVowels
+countNiceStrings :: String -> Int
+countNiceStrings = undefined -- filter doesNotContainDisallowedStrings . filter containsAtLeastOneLetterThatAppearsTwiceInARow . filter hasAtLeastThreeVowels $ S.splitOn "\n" input
+
+isNice :: String -> Bool
+isNice = undefined -- filter doesNotContainDisallowedStrings . containsAtLeastOneLetterThatAppearsTwiceInARow . hasAtLeastThreeVowels
 
 hasAtLeastThreeVowels :: String -> Bool
-hasAtLeastThreeVowels s = (L.length . filter isVowel s) >= 3
+hasAtLeastThreeVowels s = (L.length . filter isVowel $ s) >= 3
 
 isVowel :: Char -> Bool
 isVowel c
-    | c == 'a' = true
-    | c == 'e' = true
-    | c == 'i' = true
-    | c == 'o' = true
-    | c == 'u' = true
-    | otherwise = false
+    | c == 'a' = True
+    | c == 'e' = True
+    | c == 'i' = True
+    | c == 'o' = True
+    | c == 'u' = True
+    | otherwise = False
 
 containsAtLeastOneLetterThatAppearsTwiceInARow :: String -> Bool
-containsAtLeastOneLetterThatAppearsTwiceInARow = undefined
+containsAtLeastOneLetterThatAppearsTwiceInARow [] = False
+containsAtLeastOneLetterThatAppearsTwiceInARow [_] = False
+containsAtLeastOneLetterThatAppearsTwiceInARow (x:y:ys)
+  | x == y = True
+  | otherwise = containsAtLeastOneLetterThatAppearsTwiceInARow (y:ys)
 
 doesNotContainDisallowedStrings :: String -> Bool
-doesNotContainDisallowedStrings = undefined
+doesNotContainDisallowedStrings s
+  | "ab" `isInfixOf` s = False
+  | "cd" `isInfixOf` s = False
+  | "pq" `isInfixOf` s = False
+  | "xy" `isInfixOf` s = False
+  | otherwise = True
